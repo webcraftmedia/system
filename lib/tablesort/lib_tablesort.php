ce1f28a57100ab6bec755f28248b2b0c35c5109f
Messerbill <ole.hoppe@gmx.de>
2015-06-08 14:13:45 +0200

    added tablesort submodule

---
+++ lib/tablesort/lib_tablesort.php
@@ -0,0 +1,10 @@
+<?php
+namespace LIB;
+class lib_tablesort extends \LIB\lib_js{
+    public static function get_class(){
+        return self::class;}
+    public static function js_path(){
+        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'/tablesort/lib/tablesort.js');}
+    public static function version(){
+        return '<a href="https://github.com/tristen/tablesort" target="_blank">https://github.com/tristen/tablesort</a> (commit: 941a656c3e)';}
+}
