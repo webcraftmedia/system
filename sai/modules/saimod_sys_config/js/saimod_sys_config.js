883d5c3e2981a560d94c9ed2ef208eb0df85726f
Messerbill <ole.hoppe@gmx.de>
2015-06-08 21:45:26 +0200

    fixed UI: sortable config table

--- sai/modules/saimod_sys_config/js/saimod_sys_config.js
+++ sai/modules/saimod_sys_config/js/saimod_sys_config.js
@@ -1,30 +1,33 @@
 function init_saimod_sys_config() {  
-    saimod_config_tablesort("sai_mod_table_basics");
+    $("#sai_mod_table_basics").tablesorter();
 }
 
 function init_saimod_sys_config_basics() {
+    $("#sai_mod_table_basics").tablesorter();
     $('#tabs_config li').each(function(){
         $(this).removeClass('active');});
     $('#menu_tag_basics').parent().addClass('active');
-    saimod_config_tablesort("sai_mod_table_basics");
+    //saimod_config_tablesort("sai_mod_table_basics");
 }
 
 function init_saimod_sys_config_database() {
+    $("#sai_mod_table_database").tablesorter();
     $('#tabs_config li').each(function(){
         $(this).removeClass('active');});
     $('#menu_tag_database').parent().addClass('active');
-    saimod_config_tablesort("sai_mod_table_database");
+    //saimod_config_tablesort("sai_mod_table_database");
 }
 
 function init_saimod_sys_config_sai() {
+   $("#sai_mod_table_sai").tablesorter();
    $('#tabs_config li').each(function(){
         $(this).removeClass('active');});
     $('#menu_tag_sai').parent().addClass('active');
-    saimod_config_tablesort("sai_mod_table_sai");
+    //saimod_config_tablesort("sai_mod_table_sai");
 }
 
-function saimod_config_tablesort(id){
-    var table_basics = document.getElementById(id);
-    var sort = new Tablesort(table_basics, {descending: true});
-    sort.refresh();
-}
+//function saimod_config_tablesort(id){
+//    var table_basics = document.getElementById(id);
+//    var sort = new Tablesort(table_basics, {descending: true});
+//    sort.refresh();
+//}
--- sai/modules/saimod_sys_config/tpl/config_basics.tpl
+++ sai/modules/saimod_sys_config/tpl/config_basics.tpl
@@ -1,8 +1,12 @@
-<table class="table sai_table" id="sai_mod_table_basics">
-    <tr class="no-sort">
-        <th>ID <span class="glyphicon glyphicon-sort" aria-hidden="true"></span></th>
-        <th>Name <span class="glyphicon glyphicon-sort" aria-hidden="true"></span></th>
-        <th>Value <span class="glyphicon glyphicon-sort" aria-hidden="true"></span></th>
-    </tr>
-    ${basics}
+<table class="table sai_table tablesorter" id="sai_mod_table_basics">
+    <thead>
+        <tr>
+            <th>ID <span class="glyphicon glyphicon-sort" aria-hidden="true"></span></th>
+            <th>Name <span class="glyphicon glyphicon-sort" aria-hidden="true"></span></th>
+            <th>Value <span class="glyphicon glyphicon-sort" aria-hidden="true"></span></th>
+        </tr>
+    </thead>
+    <tbody>
+        ${basics}
+    </tbody>    
 </table>
