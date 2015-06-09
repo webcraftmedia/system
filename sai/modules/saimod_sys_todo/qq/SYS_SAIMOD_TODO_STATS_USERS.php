8ce8f717c394976b779fddd81ec5b5b9143ea992
rylon <rylon@web.de>
2015-06-09 03:18:11 +0200

    correted order of todo user stats - best is displayed top

--- sai/modules/saimod_sys_todo/qq/SYS_SAIMOD_TODO_STATS_USERS.php
+++ sai/modules/saimod_sys_todo/qq/SYS_SAIMOD_TODO_STATS_USERS.php
@@ -7,13 +7,17 @@ class SYS_SAIMOD_TODO_STATS_USERS extends \SYSTEM\DB\QQ {
 //pg            
 'TODO',
 //mys
-'SELECT username, 
- sum(case when state = 0 then 1 else 0 end) state_open,
- sum(case when state = 1 then 1 else 0 end) state_closed,
- COUNT(*) as count
-FROM system_todo_assign 
-LEFT JOIN system_todo ON system_todo_assign.todo = system_todo.id
-LEFT JOIN system_user ON system_todo_assign.user = system_user.id
-GROUP BY system_todo_assign.user
-ORDER BY count DESC;'
+'SELECT *, state_closed/(state_open+state_closed) as best '.
+'FROM ('.
+    'SELECT username,'.
+    ' sum(case when state = 0 then 1 else 0 end) state_open, '.
+    ' sum(case when state = 1 then 1 else 0 end) state_closed, '.
+    ' COUNT(*) as count '.
+    'FROM system_todo_assign '.
+    'LEFT JOIN system_todo ON system_todo_assign.todo = system_todo.id '.
+    'LEFT JOIN system_user ON system_todo_assign.user = system_user.id '.
+    'GROUP BY system_todo_assign.user '.
+    'ORDER BY count DESC'.
+') a '.
+'ORDER BY best DESC;'
 );}}
