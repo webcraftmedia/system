058666836f605730bc0613ad7e3b3e6abbec6af1
rylon <rylon@web.de>
2015-07-09 03:47:29 +0200

    fixed typo for sqlite extraction

--- dbd/qq/SYS_LOG_OLDEST.php
+++ dbd/qq/SYS_LOG_OLDEST.php
@@ -5,7 +5,7 @@ class SYS_LOG_OLDEST extends \SYSTEM\DB\QQ {
     public static function pqsql(){return
 'SELECT  EXTRACT(MONTH FROM time)::INTEGER as month, EXTRACT(YEAR FROM time)::INTEGER as year FROM '.\SYSTEM\DBD\system_log::NAME_PG.' ORDER BY time ASC LIMIT 1';
     }
-    public static function msql(){return
+    public static function mysql(){return
 'SELECT MONTH(time) as month, YEAR(time) as year FROM '.\SYSTEM\DBD\system_log::NAME_MYS.' ORDER BY time ASC LIMIT 1';
     }
 }
