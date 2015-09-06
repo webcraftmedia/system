95eafacf76ba3d271bcbdab57a85b08773907066
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2015-09-06 10:14:11 +0200

    sorting for newest text added in adv function

--- dbd/qq/SYS_TEXT_GET_TAG_ADV.php
+++ dbd/qq/SYS_TEXT_GET_TAG_ADV.php
@@ -7,6 +7,6 @@ class SYS_TEXT_GET_TAG_ADV extends \SYSTEM\DB\QP {
 ' LEFT JOIN system_text_tag ON system_text.id = system_text_tag.id'.
 ' LEFT JOIN system_user AS a ON system_text.author = a.id'.
 ' LEFT JOIN system_user AS ae ON system_text.author_edit = ae.id'.
-' WHERE tag = ? and lang = ?;';
+' WHERE tag = ? and lang = ? ORDER BY time_create DESC;';
     }
 }
--- dbd/qq/SYS_TEXT_SEARCH_TAG.php
+++ dbd/qq/SYS_TEXT_SEARCH_TAG.php
@@ -7,6 +7,6 @@ class SYS_TEXT_SEARCH_TAG extends \SYSTEM\DB\QP {
 ' LEFT JOIN system_text_tag ON system_text.id = system_text_tag.id'.
 ' LEFT JOIN system_user AS a ON system_text.author = a.id'.
 ' LEFT JOIN system_user AS ae ON system_text.author_edit = ae.id'.
-' WHERE tag = ? and (system_text.id LIKE ? OR system_text.text LIKE ? OR tag LIKE ?);';
+' WHERE tag = ? and (system_text.id LIKE ? OR system_text.text LIKE ? OR tag LIKE ?) ORDER BY time_create DESC;';
     }
 }
