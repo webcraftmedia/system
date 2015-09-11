0ac67a9e86d5f1e5a67bdb7ebec74f137164a44e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2015-09-11 05:03:06 +0200

    typo fix in qq

--- security/qq/SYS_SECURITY_CHECK.php
+++ security/qq/SYS_SECURITY_CHECK.php
@@ -4,13 +4,13 @@ namespace SYSTEM\SQL;
 class SYS_SECURITY_CHECK extends \SYSTEM\DB\QP {
     public static function get_class(){return \get_class();}
     public static function pqsql(){return
-'SELECT COUNT(*) as count FROM '.\SYSTEM\SQL\UserRightsTable::NAME_PG.
-' WHERE "'.\SYSTEM\SQL\UserRightsTable::FIELD_USERID.'" = $1'.
-' AND "'.\SYSTEM\SQL\UserRightsTable::FIELD_RIGHTID.'" = $2;';
+'SELECT COUNT(*) as count FROM '.\SYSTEM\SQL\system_user_to_rights::NAME_PG.
+' WHERE "'.\SYSTEM\SQL\system_user_to_rights::FIELD_USERID.'" = $1'.
+' AND "'.\SYSTEM\SQL\system_user_to_rights::FIELD_RIGHTID.'" = $2;';
     }
     public static function mysql(){return
-'SELECT COUNT(*) as count FROM '.\SYSTEM\SQL\UserRightsTable::NAME_MYS.
-' WHERE '.\SYSTEM\SQL\UserRightsTable::FIELD_USERID.' = ?'.
-' AND '.\SYSTEM\SQL\UserRightsTable::FIELD_RIGHTID.' = ?;';
+'SELECT COUNT(*) as count FROM '.\SYSTEM\SQL\system_user_to_rights::NAME_MYS.
+' WHERE '.\SYSTEM\SQL\system_user_to_rights::FIELD_USERID.' = ?'.
+' AND '.\SYSTEM\SQL\system_user_to_rights::FIELD_RIGHTID.' = ?;';
     }
 }
