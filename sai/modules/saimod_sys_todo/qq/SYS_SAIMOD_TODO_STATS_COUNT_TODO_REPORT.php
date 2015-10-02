583883cfda06a2fc724a96d76ba3ade9eada06f7
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2015-10-02 07:49:15 +0200

    todo stats report as part of the statistics

---
+++ sai/modules/saimod_sys_todo/qq/SYS_SAIMOD_TODO_STATS_COUNT_TODO_REPORT.php
@@ -0,0 +1,11 @@
+<?php
+namespace SYSTEM\SQL;
+class SYS_SAIMOD_TODO_STATS_COUNT_TODO_REPORT extends \SYSTEM\DB\QQ {
+    public static function get_class(){return \get_class();}
+    public static function pqsql(){return
+'SELECT COUNT(*) as `count` FROM system_todo WHERE state = 0 AND `type` = 2';
+    }
+    public static function mysql(){return
+'SELECT COUNT(*) as `count` FROM system_todo WHERE state = 0 AND `type` = 2';
+    }
+}
