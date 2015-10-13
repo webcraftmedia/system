13bdae9db95584f93288214d2e9393f3220ed2ed
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2015-10-14 01:48:03 +0200

    fixed todo assigned count

--- sai/modules/saimod_sys_todo/qq/SYS_SAIMOD_TODO_STATS_COUNT_TODO_NOT_ASSIGNED.php
+++ sai/modules/saimod_sys_todo/qq/SYS_SAIMOD_TODO_STATS_COUNT_TODO_NOT_ASSIGNED.php
@@ -1,10 +1,10 @@
 <?php
 namespace SYSTEM\SQL;
-class SYS_SAIMOD_TODO_STATS_COUNT_TODO_ASSIGNED extends \SYSTEM\DB\QQ {
+class SYS_SAIMOD_TODO_STATS_COUNT_TODO_NOT_ASSIGNED extends \SYSTEM\DB\QQ {
     public static function get_class(){return \get_class();}
     public static function mysql(){return
 'SELECT COUNT(*) as `count` FROM system_todo'.
 ' LEFT JOIN system_todo_assign ON system_todo.id = system_todo_assign.todo'.
-' WHERE state = 0 AND `type` = 1 AND system_todo_assign.user IS NOT NULL;';
+' WHERE state = 0 AND `type` = 1 AND system_todo_assign.user IS NULL;';
     }
 }
--- sai/modules/saimod_sys_todo/stats/todo_stats_assign.php
+++ sai/modules/saimod_sys_todo/stats/todo_stats_assign.php
@@ -3,7 +3,7 @@ namespace SYSTEM\SAI;
 
 class todo_stats_assign extends todo_stats {
     public static function stats() {
-        $count = \SYSTEM\SQL\SYS_SAIMOD_TODO_STATS_COUNT_TODO_ASSIGNED::Q1()['count'];
+        $count = \SYSTEM\SQL\SYS_SAIMOD_TODO_STATS_COUNT_TODO_NOT_ASSIGNED::Q1()['count'];
         $all = \SYSTEM\SQL\SYS_SAIMOD_TODO_STATS_COUNT_TODO_USER::Q1()['count'];
-        return new \SYSTEM\SAI\todo_stats_data('Assigned ToDos',$count,$all);}
+        return new \SYSTEM\SAI\todo_stats_data('Assigned ToDos',$all-$count,$all);}
 }
