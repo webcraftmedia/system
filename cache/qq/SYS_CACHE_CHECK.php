a0b9470dcb04b29205108d359c0cb30f6f9844e5
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-04 01:32:05 +0100

    fixed cache pg

--- cache/qq/SYS_CACHE_CHECK.php
+++ cache/qq/SYS_CACHE_CHECK.php
@@ -2,7 +2,7 @@
 namespace SYSTEM\SQL;
 class SYS_CACHE_CHECK extends \SYSTEM\DB\QP {
     public static function get_class(){return \get_class();}
-    public static function pqsql(){return          
+    public static function pgsql(){return          
 'SELECT "data" FROM system.cache'.
 ' WHERE "CacheID" = $1 AND'.
 ' "Ident" = $2;';
