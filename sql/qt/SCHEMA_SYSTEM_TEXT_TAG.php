3125cfd1b18c70df8d7c5d93b2e7993529077358
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-05 13:19:40 +0100

    setup files for pg linked in

--- sql/qt/SCHEMA_SYSTEM_TEXT_TAG.php
+++ sql/qt/SCHEMA_SYSTEM_TEXT_TAG.php
@@ -2,6 +2,9 @@
 namespace SYSTEM\SQL;
 class SCHEMA_SYSTEM_TEXT_TAG extends \SYSTEM\DB\QI {
     public static function get_class(){return \get_class();}
+    public static function files_pgsql(){
+        return array(   \SYSTEM\SERVERPATH(new \SYSTEM\PSQL(),'/qt/pgsql/schema/system_text_tag.sql'));
+    }
     public static function files_mysql(){
         return array(   \SYSTEM\SERVERPATH(new \SYSTEM\PSQL(),'/qt/mysql/schema/system_text_tag.sql'));
     }    
