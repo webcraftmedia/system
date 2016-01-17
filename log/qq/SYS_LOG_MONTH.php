0cf54538ea61a038d76ba6392cf694bfd7a1010b
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-17 20:19:56 +0100

    pg fix for log extraction

--- log/qq/SYS_LOG_MONTH.php
+++ log/qq/SYS_LOG_MONTH.php
@@ -2,7 +2,10 @@
 namespace SYSTEM\SQL;
 class SYS_LOG_MONTH extends \SYSTEM\DB\QP {
     public static function get_class(){return \get_class();}
+    public static function pgsql(){return
+'SELECT * FROM '.\SYSTEM\SQL\system_log::NAME_PG.' WHERE EXTRACT(MONTH FROM time)::INTEGER = $1 AND EXTRACT(YEAR FROM time)::INTEGER = $2 ORDER BY time ASC LIMIT 10000;';
+    }
     public static function mysql(){return
-'SELECT * FROM '.\SYSTEM\SQL\system_log::NAME_MYS.' WHERE MONTH(time) = ? AND YEAR(time) = ? ORDER BY time DESC LIMIT 10000;';
+'SELECT * FROM '.\SYSTEM\SQL\system_log::NAME_MYS.' WHERE MONTH(time) = ? AND YEAR(time) = ? ORDER BY time ASC LIMIT 10000;';
     }
 }
