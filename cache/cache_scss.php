7ae7dc87f5b4c54b7bc54115c85a7574f109cec8
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-20 03:08:39 +0100

    fixed typo

--- cache/cache_scss.php
+++ cache/cache_scss.php
@@ -3,7 +3,7 @@ namespace SYSTEM\CACHE;
 class cache_scss {
     const CACHE_SCSS = 1;
     public static function put($file,$data){
-        return \SYSTEM\CACHE\cache::put(self::CACHE_SCSS, self::ident($file), 'css', $data);}
+        return \SYSTEM\CACHE\cache::put(self::CACHE_SCSS, self::ident($file), 'CSS', $data);}
     public static function get($file,$header = false){
         return \SYSTEM\CACHE\cache::get(self::CACHE_SCSS,  self::ident($file),$header);}
     public static function ident($file){
