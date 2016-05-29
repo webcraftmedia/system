55ef099e06745bda131cbc386b115107f3899487
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-05-30 00:38:12 +0200

    fixed availability check for accounts with email uniqueness

--- security/qq/SYS_SECURITY_AVAILABLE_EMAIL.php
+++ security/qq/SYS_SECURITY_AVAILABLE_EMAIL.php
@@ -4,10 +4,10 @@ class SYS_SECURITY_AVAILABLE_EMAIL extends \SYSTEM\DB\QP {
     public static function get_class(){return \get_class();}
     public static function pgsql(){return
 'SELECT COUNT(*) as count FROM '.\SYSTEM\SQL\system_user::NAME_PG.
-' WHERE UPPER('.\SYSTEM\SQL\system_user::FIELD_USERNAME.') like UPPER($1) AND UPPER('.\SYSTEM\SQL\system_user::FIELD_EMAIL.') = UPPER($2);';
+' WHERE UPPER('.\SYSTEM\SQL\system_user::FIELD_USERNAME.') like UPPER($1) OR UPPER('.\SYSTEM\SQL\system_user::FIELD_EMAIL.') = UPPER($2);';
     }
     public static function mysql(){return
 'SELECT COUNT(*) as count FROM '.\SYSTEM\SQL\system_user::NAME_MYS.
-' WHERE UPPER('.\SYSTEM\SQL\system_user::FIELD_USERNAME.') like UPPER(?) AND UPPER('.\SYSTEM\SQL\system_user::FIELD_EMAIL.') = UPPER(?);';
+' WHERE UPPER('.\SYSTEM\SQL\system_user::FIELD_USERNAME.') like UPPER(?) OR UPPER('.\SYSTEM\SQL\system_user::FIELD_EMAIL.') = UPPER(?);';
     }
 }
