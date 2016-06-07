9c71bd702f3664f5224b020bb5f050a1bc92fcf9
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-06-07 07:45:38 +0200

    #3 docu documentation + fixes

--- docu/docu.php
+++ docu/docu.php
@@ -1,21 +1,50 @@
 <?php
+/**
+ * System - PHP Framework
+ *
+ * PHP Version 5.6
+ *
+ * @copyright   2016 Ulf Gebhardt (http://www.webcraft-media.de)
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT
+ * @link        https://github.com/webcraftmedia/system
+ * @package     SYSTEM\DOCU
+ */
 namespace SYSTEM\DOCU;
 
+/**
+ * Docu Class provided by System to register phpdocumentor configs.
+ */
 class docu {
+    /** array Variable to store all registred phpdocconfigs */
     private static $phpdocconfigs = array();
     
+    /**
+     * Register a phpdocconfig
+     *
+     * @param array $phpdocconfig Config to be registered
+     * @return null Returns null.
+     */
     public static function register($phpdocconfig){
         array_push(self::$phpdocconfigs,$phpdocconfig);}
         
+    /**
+     * Get all registered phpdocconfigs
+     *
+     * @return array Returns array with all registered phpdocconfigs.
+     */
     public static function getAll(){
         return self::$phpdocconfigs;}
     
+    /**
+     * Get a specific phpdocconfig by id
+     *
+     * @param string $id Phpdocconfig id to be found
+     * @return array Returns the specific config or throws an Error.
+     */
     public static function get($id){
         foreach(self::$phpdocconfigs as $config){
             if($config['id'] == $id){
-                return $config;}
-        }
+                return $config;}}
         throw new ERROR('PhpDocConfig for id '.$id.' not found.');
     }
-
 }
