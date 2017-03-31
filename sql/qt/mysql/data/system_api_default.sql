97d494dc078a207490c4f07089ee5e1cd6061e12
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2017-04-01 01:55:36 +0200

    added _lang to page parameters

--- sql/qt/mysql/data/system_api_default.sql
+++ sql/qt/mysql/data/system_api_default.sql
@@ -1,2 +1,3 @@
-INSERT INTO `system_api` (`ID`, `group`, `type`, `parentID`, `parentValue`, `name`, `verify`) VALUES (0, 1, 4, -1, NULL, '_escaped_fragment_', 'STRING');
-INSERT INTO `system_api` (`ID`, `group`, `type`, `parentID`, `parentValue`, `name`, `verify`) VALUES (1, 1, 0, -1, NULL, 'page', NULL);
+REPLACE INTO `system_api` (`ID`, `group`, `type`, `parentID`, `parentValue`, `name`, `verify`) VALUES (0, 1, 4, -1, NULL, '_escaped_fragment_', 'STRING');
+REPLACE INTO `system_api` (`ID`, `group`, `type`, `parentID`, `parentValue`, `name`, `verify`) VALUES (1, 1, 0, -1, NULL, 'page', NULL);
+REPLACE INTO `system_api` (`ID`, `group`, `type`, `parentID`, `parentValue`, `name`, `verify`) VALUES (90, 1, 4, -1, NULL, '_lang', 'LANG');
