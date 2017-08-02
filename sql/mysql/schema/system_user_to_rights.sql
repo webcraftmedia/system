1a703bf8e318320173e8b4ab872f624e41eb9adf
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2017-08-02 22:31:18 +0200

    #365 fixed user_to_rights table order

--- sql/mysql/schema/system_user_to_rights.sql
+++ sql/mysql/schema/system_user_to_rights.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `system_user_to_rights` (
+        `userID` INT(10) UNSIGNED NOT NULL DEFAULT '0',
 	`rightID` INT(10) NOT NULL DEFAULT '0',
-	`userID` INT(10) UNSIGNED NOT NULL DEFAULT '0',
-	PRIMARY KEY (`rightID`, `userID`)
+	PRIMARY KEY (`userID`,`rightID`)
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB;
