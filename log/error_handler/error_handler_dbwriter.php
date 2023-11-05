8981c5934fdd6222c54621642d32141060a17dc4
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-05 21:56:52 +0100

    use $_SERVER again

--- log/error_handler/error_handler_dbwriter.php
+++ log/error_handler/error_handler_dbwriter.php
@@ -27,7 +27,7 @@ class error_handler_dbwriter implements \SYSTEM\LOG\error_handler {
         try{
             $result = \SYSTEM\SQL\SYS_LOG_INSERT::QI(array(
                                                     get_class($E), $E->getMessage(), $E->getCode(), $E->getFile(), $E->getLine(), $E->getTraceAsString(),
-                                                    getenv('HTTP_X_FORWARD_FOR') ?? getenv('REMOTE_ADDR'),round(microtime(true) - \SYSTEM\time::getStartTime(),5),
+                                                    $_SERVER['HTTP_X_FORWARD_FOR'] ?? getenv('REMOTE_ADDR'),round(microtime(true) - \SYSTEM\time::getStartTime(),5),
                                                     $_SERVER["SERVER_NAME"],$_SERVER["SERVER_PORT"],$_SERVER['REQUEST_URI'], serialize($_POST),
                                                     array_key_exists('HTTP_REFERER', $_SERVER) ? $_SERVER['HTTP_REFERER'] : null,
                                                     array_key_exists('HTTP_USER_AGENT',$_SERVER) ? $_SERVER['HTTP_USER_AGENT'] : null,
@@ -54,7 +54,7 @@ class error_handler_dbwriter implements \SYSTEM\LOG\error_handler {
             if(\property_exists(get_class($E), 'todo_logged') && $E->todo_logged){                
                 return false;} //alrdy logged(this prevents proper thrown value for every system exception)
             \SYSTEM\SQL\SYS_LOG_TODO_INSERT::Q1(array(  get_class($E), $E->getMessage(), $E->getCode(), $E->getFile(), $E->getLine(), $E->getTraceAsString(),
-                                                        getenv('HTTP_X_FORWARD_FOR') ?? getenv('REMOTE_ADDR'),round(microtime(true) - \SYSTEM\time::getStartTime(),5),date('Y-m-d H:i:s', time()),
+                                                        $_SERVER['HTTP_X_FORWARD_FOR'] ?? getenv('REMOTE_ADDR'),round(microtime(true) - \SYSTEM\time::getStartTime(),5),date('Y-m-d H:i:s', time()),
                                                         $_SERVER["SERVER_NAME"],$_SERVER["SERVER_PORT"],$_SERVER['REQUEST_URI'], serialize($_POST),
                                                         array_key_exists('HTTP_REFERER', $_SERVER) ? $_SERVER['HTTP_REFERER'] : null,
                                                         array_key_exists('HTTP_USER_AGENT',$_SERVER) ? $_SERVER['HTTP_USER_AGENT'] : null,
